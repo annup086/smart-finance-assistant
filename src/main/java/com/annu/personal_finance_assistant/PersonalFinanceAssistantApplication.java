92e01b20344b63d5bc6fc9b0918763a681cd1834
annup086 <annupathippallil@gmail.com>
2026-05-10 14:13:37 +1000

    Initial Spring Boot project setup

---
+++ src/main/java/com/annu/personal_finance_assistant/PersonalFinanceAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.annu.personal_finance_assistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalFinanceAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalFinanceAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/annu/personal_finance_assistant/PersonalFinanceAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annu.personal_finance_assistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalFinanceAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
